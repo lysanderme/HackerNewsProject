48676146c0caceb9bc373352b39f7912f7726b2d
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2023-12-13 10:00:47 +0000

    LME commit

--- HackerNewsProject/ContentView.swift
+++ HackerNewsProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, orld!")
+            Text("Hello, rld!")
         }
         .padding()
     }
