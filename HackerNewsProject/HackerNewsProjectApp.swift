11c254f7842365f2cfd6b5e0ed6030cd88242ef4
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2023-12-13 09:57:10 +0000

    Initial Commit

---
+++ HackerNewsProject/HackerNewsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsProjectApp.swift
+//  HackerNewsProject
+//
+//  Created by Myrddin-Evans, Lysander (JD) on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
